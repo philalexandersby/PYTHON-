c928966e4a6f31174f8d3d4dbae1fd3689a61bb6
philalexandersby <136056923+philalexandersby@users.noreply.github.com>
2024-08-15 11:03:04 +0800

    Add files via upload

---
+++ 第13章常用模块.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 11:22:43 2024
+
+@author: Administrator
+"""
+
+#import math
+#print(math.ceil(4.5))
+#print(math.floor(4.5))
+#print(round(4.5))
+#print(math.log(634646346,2))
+#print(math.sqrt(1434343413))
+#print(math.pow(34343,4))
+#print(math.sin(46466))
+#print(math.cos(22424))
+#print(math.tan(3343434434344))
+#print(math.asin(0.999999999999999999999999999999999999999999999999999999))
+#print(math.acos(0.999999999999999999999999999999999999999999))
+#print(math.atan(3743473))
+#print(math.degrees(3464646))
+#print(math.radians(443434))
+#import random
+#print(random.random())
+#print(random.randrange(1656))
+#print(random.randrange(0,10,5))
+#print(random.randint(100, 102))
+#print('0.0<x<1.0的随机数')
+#for i in range(0,10):
+#    x=random.random()
+#    print(x)
+   
+#print('0.0<x<5.0的随机数')
+#for i in range(0,5):
+#    s=random.randrange(5)
+#    print(s,end='\n\n')
+#import random
+#print('打印0到10之间的随机数,打印10次')
+#for i in range(0,10):
+#    x=random.randrange(0,10)
+#    print(x,end='\n\n')
+#import random
+#print('使用randint函数打印5到10之间的随机整数10次')
+#for i in range(0,10):
+#    x=random.randrange(5,10)
+#    print(x,end='\n')
+#import random
+#print('打印大于等于0小于1之间的浮点数字')
+#for i in range(0,434):
+#    x=random.random()
+#    print(x,end='')
+#print('打印大于等于0小于100之间的随机整数')
+#for i in range(0,634):
+#    s=random.randrange(100)
+#    print(s,end='')
+#print('打印大于等于10小于100之间的随机整数')
+#for i in range(0,454):
+#    y=random.randrange(10,100)
+#    print(y,end='')
+#print('打印大于等于10小于等于20之间的随之整数')
+#for i in range(0,543):
+#    z=random.randint(10,20)
+#    print(z,end='')
+#import datetime
+#dt=datetime.datetime(2018,8,9)
+#print(dt)
+#print(datetime.datetime.today())
+#print(datetime.datetime.now())    
+#print(datetime.datetime.utcnow())
+#timestamp=11111111111
+#print(datetime.datetime.fromtimestamp(timestamp))
+#delta=datetime.timedelta(10)
+#import datetime
+#d=datetime.date.today()
+#delta=datetime.timedelta(10)
+#d+=delta
+#print(d)
+import datetime
+d=datetime.date.today()
+delta=datetime.timedelta(weeks=5)
+d-=delta
+print(d)
+
+    
+    
+
